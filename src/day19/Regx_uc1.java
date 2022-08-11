8e29aa87e17757f403a443dbc7b33af9b92dfb4f
Your Name <you@example.com>
2022-08-11 18:50:01 +0530

    program to check whether Name - Last name starts with Cap and has minimum 3 characters

--- src/day19/Regx_uc1.java
+++ src/day19/Regx_uc1.java
@@ -9,11 +9,20 @@ public class Regx_uc1 {
 		Scanner sc =  new Scanner(System.in);
 		System.out.println("Enter the first name");
 		String firstname = sc.next();
+		System.out.println("Enter the last name");
+		String lastname = sc.next();
 		/**
 		 * checking if the given name starts with capital letter and has min 3 letters
 		 */
 		boolean result = Pattern.matches("[A-Z][a-z]{3,}", firstname);
-		if(result) {
+		/**
+		 * checking if the given name starts with capital letter and has min 3 letters
+		 */
+		boolean result2 = Pattern.matches("[A-Z][a-z]{3,}", lastname);
+		/**
+		 * if two cases are true print valid else invalid
+		 */
+		if(result && result2) {
 			System.out.println("valid");
 		}
 		else {
