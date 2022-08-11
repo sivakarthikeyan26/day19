adf87a4de7d542a70ead04b8fa78cf36ca4cc7f5
Your Name <you@example.com>
2022-08-11 18:46:01 +0530

    First name starts with Cap and has minimum 3 characters

---
+++ src/day19/Regx_uc1.java
@@ -0,0 +1,25 @@
+package day19;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Regx_uc1 {
+	
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		System.out.println("Enter the first name");
+		String firstname = sc.next();
+		/**
+		 * checking if the given name starts with capital letter and has min 3 letters
+		 */
+		boolean result = Pattern.matches("[A-Z][a-z]{3,}", firstname);
+		if(result) {
+			System.out.println("valid");
+		}
+		else {
+			System.out.println("invalid");
+		}
+		sc.close();
+	}
+
+}
